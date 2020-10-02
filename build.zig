01ff2dce44bc4495116c5b29fadc0c5bcacb0ad9
Leon Henrik Plickat <leonhenrik.plickat@stud.uni-goettingen.de>
2020-10-02 15:53:08 +0200

    Create river-layout-unstable-v1 protocol

--- build.zig
+++ build.zig
@@ -152,7 +152,6 @@ const ScdocStep = struct {
         "doc/river.1.scd",
         "doc/riverctl.1.scd",
         "doc/rivertile.1.scd",
-        "doc/river-layouts.7.scd",
     };
 
     builder: *zbs.Builder,
